50f4325d0f28f4ca4ba9ff4a09b72426ef8055d2
Mohima <mohimapersonal@gmail.com>
2025-11-15 22:57:02 +0600

    Add files via upload

---
+++ roman number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter any number ");
+    scanf("%d",&n);
+    switch(n)
+    {
+
+        case 1: printf("I");break;
+        case 2: printf("II");break;
+        case 3: printf("III");break;
+        case 4: printf("IV");break;
+        case 5: printf("v");break;
+        case 6: printf("VI");break;
+        case 7: printf("VII");break;
+        case 8: printf("VIII");break;
+        case 9: printf("IX");break;
+        case 10:printf("X");
+        break;
+
+        default: printf("out of range");
+
+    }
+
+}
+
